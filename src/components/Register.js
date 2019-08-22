b00917a7b1289f0bfce530e10a56dcb089be0a8a
rochafialvin <rochafi.dev@gmail.com>
2019-08-22 11:42:57 +0700

    Refactor form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,34 +1,44 @@
 import React, { Component } from 'react'
 
 class Register extends Component {
+
+
+    onRegisterClick = () => {
+        
+    }
+
     render() {
         return (
             <div>
-                <div className='col-sm-4 mx-auto card'>
+                <div className='col-sm-4 mx-auto card mt-5'>
                     <div className='card-body'>
                         <div className="card-title border-bottom border-secondary">
                             <h1>Register</h1>
                         </div>
-                        <div className="card-title ">
-                            <h4>Username</h4>
-                        </div>
-                        <form className='input-group'>
+                        
+                        <form className='form-group'>
+                            <div className="card-title ">
+                                <h4>Username</h4>
+                            </div>
                             <input type='text' className='form-control'/>
-                        </form>
 
-                        <div className="card-title ">
-                            <h4>Email</h4>
-                        </div>
-                        <form className='input-group'>
-                            <input type='text' className='form-control'/>
-                        </form>
-
-                        <div className="card-title ">
-                            <h4>Password</h4>
-                        </div>
-                        <form className='input-group'>
+                            <div className="card-title ">
+                                <h4>Email</h4>
+                            </div>
                             <input type='password' className='form-control'/>
+
+                            <div className="card-title ">
+                                <h4>Password</h4>
+                            </div>
+                            <input type='password' className='form-control'/>
+
                         </form>
+                        
+                        <button 
+                            className="btn-block btn btn-outline-primary mt-2"
+                            onClick={this.onRegisterClick}
+                        >Register</button>
+                        
                     </div>
                 </div>
             </div>
